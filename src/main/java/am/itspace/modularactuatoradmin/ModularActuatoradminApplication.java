c273498faed3ec569b8b086fbdd6ca55f873fc05
sako01 <sargsyansak@gmail.com>
2019-06-13 09:23:25 +0400

    Initial commit

---
+++ src/main/java/am/itspace/modularactuatoradmin/ModularActuatoradminApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.modularactuatoradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularActuatoradminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModularActuatoradminApplication.class, args);
+    }
+
+}
